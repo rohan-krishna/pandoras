30c6f9a1eac2921271ea4e4ddc22f1c8cdcd3859
Rohan Krishna <phonemg30993@gmail.com>
2018-06-27 18:43:59 +0530

    React-Redux-Thunk workflow added.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,8 @@
 
 @section('content')
 <div class="ui container">
-    <div class="ui segment blue">
-        <h3>Dashboard</h3>
-        <hr>
-        <p>Welcome!</p>
+    <div id="itemsApp">
+        
     </div>
 </div>
 @endsection
