140ab8a55274c0c9a89618859921e82fac615f97
rohan-krishna <phonemg30993@gmail.com>
2019-03-27 14:14:11 +0530

    Pushing for Rejo to test OAuth.

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,24 @@ Route::prefix('oauth')->group(function() {
     Route::get('/',function() {
         return view('oauth');
     });
+
+    Route::get("test", function() {
+        $http = new \GuzzleHttp\Client;
+
+        $response = $http->post('http://your-app.com/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => 'client-id',
+                'client_secret' => 'client-secret',
+                'username' => 'taylor@laravel.com',
+                'password' => 'my-password',
+                'scope' => '',
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
+
+    });
 });
 
 Route::prefix('playground')->group(function() {
